7080e938e2fd4064f7e321aaf666ff363e6bbc7c
Pedro Feltrin <pedrofeltrin45@gmail.com>
2021-11-27 16:18:28 -0300

    feat: create Header component; lint project

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import classes from "../styles/components/Header.module.css";
+
+export const Header = () => (
+	<header className={classes.header}>
+		<Image
+			src="/assets/ball.svg"
+			alt="Bola de futebol"
+			width={72}
+			height={72}
+		/>
+		<h1 className={classes.title}>Gerenciador de Fut</h1>
+	</header>
+);
